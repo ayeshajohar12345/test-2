970549c58c571992efcc4cda31c790ab07ac375c
ayeshajohar12345 <38943033+ayeshajohar12345@users.noreply.github.com>
2018-10-08 17:49:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let alphabets = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b]
+function att(alphabets) {
+    document.form.textview.value = document.form.textview.value+alphabets;
+}
+function clean(){
+    document.form.textview.value = "";
+}
